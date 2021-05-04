45c6543191c59583e65a267bb1b5592b196c2186
gaorunding <18795880709@163.com>
2021-05-04 22:03:47 +0800

    5月2日剑指offer刷题

---
+++ algorithm/src/main/java/offer/Day050201.java
@@ -0,0 +1,63 @@
+package offer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-02 19:29
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-02 gaorunding v1.0.0 修改原因
+ * 题目描述：输入n个整数，找出其中最小的K个数。
+ * 思路：先将前K个数放入数组，进行堆排序，若之后的数比它还小，则进行调整
+ */
+public class Day050201 {
+
+    public ArrayList<Integer> getLeastNumbersSolution(int[] input,int k){
+        ArrayList<Integer> list=new ArrayList<>();
+        if (input==null||k<=0||k>input.length){
+            return list;
+        }
+        int[] kArray= Arrays.copyOfRange(input,0,k);
+        buildHeap(kArray);
+        for (int i = k; i < input.length; i++) {
+            if (input[i]<kArray[0]){
+                kArray[0]=input[i];
+                maxHeap(kArray,0);
+            }
+        }
+        return list;
+    }
+
+    public void buildHeap(int[] input){
+        for (int i = input.length/2-1; i >=0 ; i--) {
+            maxHeap(input,i);
+        }
+    }
+
+    private void maxHeap(int[] array, int i) {
+        int left = 2 * i + 1;
+        int right = left + 1;
+        int largest = i;
+        if (left < array.length && array[left] > array[i]) {
+            largest = left;
+        }
+        if (right < array.length && array[right] > array[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+            maxHeap(array, largest);
+        }
+    }
+
+
+}
---
+++ algorithm/src/main/java/offer/Day050202.java
@@ -0,0 +1,32 @@
+package offer;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-02 20:46
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-02 gaorunding v1.0.0 修改原因
+ * 题目描述：输入一个整型数组，数组中有正数也有负数，数组中一个或连续的多个整数组成一个子数组，求连续子数组的最大和
+ * 思路：若和小于0，则将最大和置为当前值，否则计算最大和。
+ */
+public class Day050202 {
+
+    public int maxSubArray(int[] nums){
+        if (nums==null||nums.length==0){
+            return 0;
+        }
+        int sum=0;
+        int result=nums[0];
+        for (int num : nums) {
+            sum=sum<0?num:sum+num;
+            result=Math.max(result,sum);
+        }
+        return result;
+    }
+
+}
