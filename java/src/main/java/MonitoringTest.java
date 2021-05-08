358669bb0c223836ff237095a1fb809e5a0ad065
gaorunding <18795880709@163.com>
2021-05-08 21:55:43 +0800

    jvm测试各种监控工具代码

---
+++ java/src/main/java/MonitoringTest.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-08 14:30
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-08 gaorunding v1.0.0 修改原因
+ */
+public class MonitoringTest {
+    static class OOMObject {
+        public byte[] placeholder = new byte[64 * 1024];
+        public static void fillHeap(int num) throws InterruptedException {
+            List<OOMObject> list=new ArrayList<>();
+            for (int i = 0; i <num ; i++) {
+                Thread.sleep(500);
+                list.add(new OOMObject());
+            }
+            System.gc();
+        }
+
+        public static void main(String[] args) throws InterruptedException {
+            fillHeap(1000);
+        }
+    }
+}
---
+++ java/src/main/java/MonitoringThreadLockTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description: 检测死锁
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-08 16:38
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-08 gaorunding v1.0.0 修改原因
+ */
+public class MonitoringThreadLockTest {
+    static class SynAddRunable implements Runnable{
+        int a,b;
+        public SynAddRunable(int a,int b){
+            this.a=a;
+            this.b=b;
+        }
+        @Override
+        public void run() {
+            synchronized (Integer.valueOf(a)){
+                synchronized (Integer.valueOf(b)){
+                    System.out.println(a+b);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(new SynAddRunable(1,2)).start();
+            new Thread(new SynAddRunable(2,1)).start();
+        }
+    }
+}
