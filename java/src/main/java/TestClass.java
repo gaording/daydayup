acc745acf2b84982de5123331787363439f8e25e
gaorunding <18795880709@163.com>
2021-05-11 18:30:30 +0800

    redis io、过期key、内核态和用户态、java线程、直接IO、缓存IO、key过期和淘汰策略、管道、事务

---
+++ java/src/main/java/TestClass.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-10 22:37
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-10 gaorunding v1.0.0 修改原因
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
