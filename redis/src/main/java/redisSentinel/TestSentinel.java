8832f26dbe5b9386ef4131e29d6238f58ebcb168
gaorunding <18795880709@163.com>
2021-05-15 08:12:39 +0800

    增加log4j日志配置，哨兵集群测试更改

---
+++ redis/src/main/java/redisSentinel/TestSentinel.java
@@ -0,0 +1,59 @@
+package redisSentinel;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-14 15:53
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-14 gaorunding v1.0.0 修改原因
+ */
+public class TestSentinel {
+    static JedisSentinelPool pool;
+
+    static {
+        String masterName="mymaster";
+        Set<String> setinels=new HashSet<>();
+        setinels.add(new HostAndPort("localhost",26379).toString());
+//        setinels.add(new HostAndPort("localhost",26380).toString());
+//        setinels.add(new HostAndPort("localhost",26381).toString());
+        GenericObjectPoolConfig poolConfig=new GenericObjectPoolConfig();
+        poolConfig.setMaxWaitMillis(1000);
+        pool = new JedisSentinelPool(masterName, setinels,poolConfig);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Jedis jedis = null;
+        //TODO 这里是main线程和masterListener线程同时操作initpool
+        // 导致偶尔获取资源的时候masterListener线程虽然更改了volatile修饰的currentHostPort值，
+        // 但是还未能初始化完internalPool，main线程获取资源的时候internalPool为null，出现了npe异常
+        // 让主线程sleep 1s，保证internalpool已被初始化完毕，就没这个问题了
+        Thread.sleep(1000);
+        try {
+            jedis = pool.getResource();
+            System.out.println(jedis.get("sentinel"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+            System.out.println("end");
+        }
+
+
+    }
+
+}
