2b61b64e8cc0f1652b7c292c8dfb1acf366a63bb
gaorunding <18795880709@163.com>
2021-05-26 09:29:52 +0800

    mybatis测试

---
+++ mybatis/src/main/java/User.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-19 14:50
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-19 gaorunding v1.0.0 修改原因
+ */
+public class User {
+}
