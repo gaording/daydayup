5cc59be8ecc43299ad93225e402c84d0a1365fc6
gaorunding <18795880709@163.com>
2021-05-26 09:28:54 +0800

    java8函数式接口

---
+++ springboot/src/main/java/com/example/demo/entity/JobFunction.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-23 10:09
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-23 gaorunding v1.0.0 修改原因
+ */
+@FunctionalInterface
+public interface JobFunction {
+    void execute();
+}
