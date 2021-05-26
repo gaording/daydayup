9a407ac31290ba0b3ccc89f19b7a567c3a4029ef
gaorunding <18795880709@163.com>
2021-05-26 11:39:01 +0800

    两个线程使用wait/notify交替打印，三个线程使用lock交替打印

---
+++ java/src/main/java/jvm/waitNotify/WaitNotifyQueue.java
@@ -0,0 +1,71 @@
+package jvm.waitNotify;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-05-18 17:47
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-05-18 gaorunding v1.0.0 修改原因
+ */
+public class WaitNotifyQueue {
+
+
+    public static void main(String[] args) {
+        final Object o = new Object();
+        new Thread(() -> {
+            for (int i = 1; i < 100; i += 3) {
+                synchronized (o) {
+                    System.out.println(Thread.currentThread().getName() + ":" + i);
+                    o.notify();
+                    try {
+                        o.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i = 2; i < 100; i += 3) {
+                synchronized (o) {
+                    System.out.println(Thread.currentThread().getName() + ":" + i);
+                    o.notify();
+                    try {
+                        o.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public synchronized void print() {
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+            this.notify();
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void print2() {
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.println(i);
+            this.notify();
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
