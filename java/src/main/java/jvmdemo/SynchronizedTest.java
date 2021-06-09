cbfce2848de3c9830604b5d2fa7e11725cbd2df5
gaorunding <18795880709@163.com>
2021-06-09 14:48:32 +0800

    方法显式synchronized会在描述符中加入synchronized参数，然后jvm会在执行方法时修改对应对象的对象头，存入上锁的线程，锁计数器+1，执行完毕后锁计数器-1。方法内synchronized块会在字节码中加入monitorenter和monitorexit指令

---
+++ java/src/main/java/jvmdemo/SynchronizedTest.java
@@ -0,0 +1,29 @@
+package jvmdemo;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description: 同步锁字节码测试
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-06-08 23:04
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-06-08 gaorunding v1.0.0 修改原因
+ */
+public class SynchronizedTest {
+    private int i = 0;
+
+    public synchronized void add() {
+        i++;
+    }
+
+    private Object object = new Object();
+
+    public void subtract() {
+        synchronized (object) {
+            i--;
+        }
+    }
+}
