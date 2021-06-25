770fe57c8f9e0fac7120baa43abf37404fa6472b
gaorunding <18795880709@163.com>
2021-06-25 15:25:26 +0800

    retention、target注解生成

---
+++ java/src/main/java/crazyjava/annotations/custom/Test.java
@@ -0,0 +1,8 @@
+package crazyjava.annotations.custom;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+}
