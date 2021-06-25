f96fd9473c72dc98116b8ee69ca59de55c28adb8
gaorunding <18795880709@163.com>
2021-06-25 15:18:54 +0800

    类初始化：反射、new对象、子类初始化、静态变量被调用都会导致初始化

---
+++ java/src/main/java/crazyjava/annotations/inherite/Base.java
@@ -0,0 +1,17 @@
+package crazyjava.annotations.inherite;
+
+/**
+ * Copyright: Copyright (c) 2021 IWhaleCloud
+ *
+ * @program: forOffer
+ * @description:
+ * @version: v1.0.0
+ * @author: gaorunding
+ * @date: 2021-06-23 06:52
+ * <p>
+ * Modification History: Date Author Version Description
+ * ------------------------------------------------------------ 2021-06-23 gaorunding v1.0.0 修改原因
+ */
+@Inheritable
+public class Base {
+}
