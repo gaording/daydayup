a3968699fb589ea38bff015bfa482a6318156121
gaorunding <18795880709@163.com>
2021-06-25 15:59:42 +0800

    类静态变量、静态方法、实例变量、实例方法的加载顺序，实质就是cinit和init方法的调用顺序

--- java/src/main/java/jvm/TestClass.java
+++ java/src/main/java/jvm/TestClass.java
@@ -11,12 +11,23 @@ package jvm;
  * <p>
  * Modification History: Date Author Version Description
  * ------------------------------------------------------------ 2021-05-10 gaorunding v1.0.0 修改原因
+ * 类初始化之后实例变量加载
+ * <cinit>之后调用了<init>,静态变量和代码块就都赋值了，然后实例变量再调用方法invokevirtual
  */
 public class TestClass {
 
-    private int m;
+    private static String s1 = TestA.testB();
+    private String s2 = TestA.testA();
 
-    public int inc() {
-        return m + 1;
+    static {
+        s1 = TestA.testC();
+    }
+
+    public TestClass() {
+        s2 = TestA.testD();
+    }
+
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
     }
 }
