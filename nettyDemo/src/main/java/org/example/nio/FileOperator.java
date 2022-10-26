075a51ecc59a70ff305229e79eeb6df76a4a7d21
gaorunding1 <gaorunding1@jd.com>
2022-10-26 13:56:17 +0800

    测试mappedByteBuffer，操作系统不需要额外拷贝。直接利用mmap机制修改内核空间内存。适用于大文件

--- nettyDemo/src/main/java/org/example/nio/FileOperator.java
+++ nettyDemo/src/main/java/org/example/nio/FileOperator.java
@@ -1,19 +1,26 @@
 package org.example.nio;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
  * @program: daydayup
  * @date: 2022/10/25
  * @author: gaorunding1
- * @description: 文件操作(测试两个file通过channel通过bytebuffer交互)
+ * @description: 文件操作
  **/
 public class FileOperator {
     public static void main(String[] args) throws IOException {
+        test3();
+    }
+
+
+    /**
+     * 测试两个file通过channel通过bytebuffer交互
+     */
+    private static void test1() throws IOException {
         FileInputStream fileInputStream = new FileInputStream("nettyDemo/1.txt");
         FileChannel channel1 = fileInputStream.getChannel();
         FileOutputStream fileOutputStream = new FileOutputStream("nettyDemo/2.txt");
@@ -21,12 +28,37 @@ public class FileOperator {
         ByteBuffer byteBuffer = ByteBuffer.allocate(8);
         int read;
         while ((read = channel1.read(byteBuffer)) != -1) {
-            System.out.println(new String(byteBuffer.array(),0,read));
+            System.out.println(new String(byteBuffer.array(), 0, read));
             byteBuffer.flip();
             channel2.write(byteBuffer);
             byteBuffer.clear();
         }
         fileInputStream.close();
         fileOutputStream.close();
+    }
+
+    /**
+     * 测试两个file通过channel直接交互
+     */
+    private static void test2() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("nettyDemo/1.txt");
+        FileChannel channel1 = fileInputStream.getChannel();
+        FileOutputStream fileOutputStream = new FileOutputStream("nettyDemo/2.txt");
+        FileChannel channel2 = fileOutputStream.getChannel();
+        channel2.transferFrom(channel1, 0, new File("nettyDemo/1.txt").length());
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+    /**
+     * 测试两个file通过channel直接交互
+     */
+    private static void test3() throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile("nettyDemo/1.txt", "rw");
+        FileChannel channel = randomAccessFile.getChannel();
+        MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0,8 );
+        map.put(0,(byte) 'H');
+        map.put(7,(byte) '9');
+        randomAccessFile.close();
     }
 }
